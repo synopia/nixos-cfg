8034dd5b5d397b8213e3d9661ce1f1a941947d81
Paul Fritsche <paul.fritsche@gmail.com>
2026-07-02 00:31:46 +0200

    Audio tools

--- modules/nixos/audio.nix
+++ modules/nixos/audio.nix
@@ -1,11 +1,18 @@
+{ pkgs, ... }:
 {
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
+  environment.systemPackages = with pkgs; [
+    alsa-utils
+    easyeffects
+    pwvucontrol
+  ];
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+    wireplumber.enable = true;
     # If you want to use JACK applications, uncomment this
     #jack.enable = true;
 
