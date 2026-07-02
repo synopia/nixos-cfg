79490570755f2cb1eb87644eb6483eabbb01296d
Paul Fritsche <paul.fritsche@gmail.com>
2026-07-02 07:24:18 +0200

    Add Qt QML development tools

--- modules/home-manager/development.nix
+++ modules/home-manager/development.nix
@@ -10,6 +10,23 @@
 
     docker-compose
 
+    qtcreator
+
+    qt6.qtbase
+    qt6.qtdeclarative # Provides qmlls, qmlformat, qmllint, qmlscene, and other QML tools.
+    qt6.qtshadertools
+    qt6.qttools
+    qt6.qtwayland
+    qt6.qt5compat
+
+    qt5.qtbase
+    qt5.qtbase.bin
+    qt5.qtdeclarative
+    qt5.qtdeclarative.bin
+    qt5.qttools
+    qt5.qttools.bin
+    qt5.qtwayland
+
     jetbrains.idea
   ];
 
