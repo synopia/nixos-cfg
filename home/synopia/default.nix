c3ba4c29eaa0ee563a1aafd248e4fc829930c12c
Paul Fritsche <paul.fritsche@gmail.com>
2026-07-04 08:10:06 +0200

    matugen

--- home/synopia/default.nix
+++ home/synopia/default.nix
@@ -59,7 +59,8 @@
       drun-display-format = "{name}";
     };
   };
-  home.file."/.config/rofi/menu.rasi".source = lib.mkOutOfStoreSymlink "/home/synopia/dev/menu.rasi";
+  home.file."/.config/rofi/menu.rasi".source =
+    lib.file.mkOutOfStoreSymlink "/home/synopia/dev/menu.rasi";
 
   home.packages = with pkgs; [
     htop
