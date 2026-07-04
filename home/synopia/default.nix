fe7fb99e3cb99c42c056a174cff95d4f85fc2e29
Paul Fritsche <paul.fritsche@gmail.com>
2026-07-04 07:24:09 +0200

    matugen

--- home/synopia/default.nix
+++ home/synopia/default.nix
@@ -50,15 +50,18 @@
     };
   };
 
-  programs.rofi = {
-    enable = true;
-    modes = [ "drun" ];
-    extraConfig = {
-      show-icons = true;
-      icon-theme = "Papirus-Dark";
-      drun-display-format = "{name}";
-    };
-  };
+  environment.systemPackages = with pkgs; [
+    rofi
+  ];
+  # programs.rofi = {
+  #   enable = true;
+  #   modes = [ "drun" ];
+  #   extraConfig = {
+  #     show-icons = true;
+  #     icon-theme = "Papirus-Dark";
+  #     drun-display-format = "{name}";
+  #   };
+  # };
   home.packages = with pkgs; [
     htop
     btop
