752248b48ce262bf3bc80d888aeb5c775f692ff9
Paul Fritsche <paul.fritsche@gmail.com>
2026-07-04 15:06:01 +0200

    Rework

--- modules/apps/zed.nix
+++ modules/apps/zed.nix
@@ -17,7 +17,7 @@ in
   };
 
   config = mkIf cfg.enable {
-    programs.zed-editor = {
+    syncon.home.extraOptions.programs.zed-editor = {
       enable = true;
       extensions = [
         "nix"
